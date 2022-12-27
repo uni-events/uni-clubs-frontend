8c19b6ab69c2011e14022a7984b09d65ded3ee5b
HARI-PRMD <haripramodb@gmail.com>
2022-12-27 18:14:56 +1100

    Created background scroll effect

--- src/Screens/Club/info.tsx
+++ src/Screens/Club/info.tsx
@@ -1,27 +1,26 @@
+import logo from "../../Assets/RGS-logo.png";
 const ClubInfo = () => {
+  const longStr =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vehicula vitae massa in rutrum. Maecenas nec consectetur libero. Fusce in dapibus tortor, sit amet faucibus arcu. Suspendisse vel nunc turpis. Pellentesque justo risus, gravida et metus id, vestibulum rutrum libero. Donec molestie vehicula ornare. Quisque ultrices odio ac ante tincidunt egestas.";
   return (
     <>
       <div className=" mt-0">
         <div
-          className=" bg-fixed h-40"
+          className=" bg-fixed h-40 md:h-96"
           style={{
             backgroundImage: `url(${require("../../Assets/page-banner.png")})`,
           }}
-        ></div>
-        <div className="bg-white p-4 sm:p-8">
-          <div className="font-inter font-extrabold text-2xl text-black tracking-tight">
-            My trip to the summit
-          </div>
-          <div className="mt-1 font-medium text-sm text-slate-500">
-            November 16, 2021 · 4 min read
-          </div>
-          <p className="mt-4 leading-7 text-slate-500">
-            Maybe we can live without libraries, people like you and me. Maybe.
-            Sure, we're too old to change the world, but what about that kid,
-            sitting down, opening a book, right now, in a branch at the local
-            library and finding drawings of pee-pees and wee-wees on the Cat in
-            the Hat and the Five Chinese Brothers? Doesn't HE deserve better?
-          </p>
+        />
+        <div className="bg-WhiteBG dark:bg-BlackBG p-4 mx-6 md:mx-12">
+          <img
+            className="-translate-y-12 h-24 w-24 md:h-32 md:w-32 md:-translate-y-24 bg-center object-cover"
+            src={logo}
+            alt="logo"
+          />
+          <h1 className="text-2xl font-bold text-black dark:text-WhiteBG">
+            Club Name Club Name (CN CN)
+          </h1>
+          <p className=" text-md text-black dark:text-WhiteBG">{longStr}</p>
         </div>
       </div>
     </>
