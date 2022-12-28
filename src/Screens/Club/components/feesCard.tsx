7c11c4c019d5042e9fca6fce45174ba5b0270c31
HARI-PRMD <haripramodb@gmail.com>
2022-12-28 19:42:33 +1100

    added join on arc button to fees section

--- src/Screens/Club/components/feesCard.tsx
+++ src/Screens/Club/components/feesCard.tsx
@@ -5,11 +5,14 @@ const FeesCard = () => {
         <h1 className="text-2xl font-bold text-black dark:text-WhiteBG">
           Membership
         </h1>
-        <div className="flex flex-col space-y-2 px-2">
+        <div className="flex flex-col space-y-2 px-2 pb-2">
           <h1>Arc Member: $5</h1>
           <h1>Non-Arc Member: $10</h1>
           <h1>Associate Member: $10</h1>
         </div>
+        <button className="h-fit w-full py-2 px-4 rounded-lg bg-Green hover:bg-DarkGreen text-white font-bold text-center">
+          Join on Arc
+        </button>
       </div>
     </>
   );
