3dcb3c0a08aeddd6b7c4e19d3bb0e5dd864c709d
HARI-PRMD <haripramodb@gmail.com>
2022-12-30 18:37:42 +1100

    idk what changes i made last night

--- src/Screens/Events/eventPage.tsx
+++ src/Screens/Events/eventPage.tsx
@@ -1,38 +1,17 @@
 import Navbar from "../../Components/navbar";
-import SingleEventCard from "./components/eventCard";
+import EventOfTheDay from "./components/eventsOfDay";
+
+import TrendingEvents from "./components/trendingEvents";
 
 const EventPage = () => {
   return (
     <>
       <div className="w-screen h-screen bg-WhiteBG dark:bg-BlackBG  overflow-x-hidden">
         <Navbar />
-
-        <div className="w-full h-full max-w-7xl mx-auto">
-          <div className="space-y-4 mt-4">
-            <h1 className="text-2xl font-bold text-black dark:text-WhiteBG">
-              Trending Events
-            </h1>
-            <div className="flex flex-row space-x-4 pb-2 overflow-x-scroll scrollbar-thin scrollbar-track-transparent scrollbar-thumb-DarkBlueGrey dark:scrollbar-thumb-BlueGrey scrollbar-thumb-rounded-lg">
-              <SingleEventCard />
-              <SingleEventCard />
-              <SingleEventCard />
-            </div>
-          </div>
-          <div className="space-y-4 mt-8">
-            <h1 className="text-2xl font-bold text-black dark:text-WhiteBG">
-              Events of the Day
-            </h1>
-            <div className="flex flex-row space-x-4 pb-2 overflow-x-scroll scrollbar-thin scrollbar-track-transparent scrollbar-thumb-DarkBlueGrey dark:scrollbar-thumb-BlueGrey scrollbar-thumb-rounded-lg">
-              <SingleEventCard />
-              <SingleEventCard />
-              <SingleEventCard />
-              <SingleEventCard />
-              <SingleEventCard />
-              <SingleEventCard />
-              <SingleEventCard />
-              <SingleEventCard />
-              <SingleEventCard />
-            </div>
+        <div className="w-full  h-full max-w-7xl mx-auto p-4">
+          <div className="flex flex-col ">
+            <TrendingEvents />
+            {/* <EventOfTheDay /> */}
           </div>
         </div>
       </div>
