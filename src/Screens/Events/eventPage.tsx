f95a2b9002933e8de5fc66683a55c3be15ddbcc1
HARI-PRMD <haripramodb@gmail.com>
2023-01-02 22:02:01 +1100

    Slider for filter section

--- src/Screens/Events/eventPage.tsx
+++ src/Screens/Events/eventPage.tsx
@@ -1,5 +1,7 @@
 import Navbar from "../../Components/navbar";
-import EventOfTheDay from "./components/eventsOfDay";
+import SearchBar from "../Discover/components/searchbar";
+// import EventOfTheDay from "./components/eventsOfDay";
+import EventsFilter from "./components/filter";
 
 import TrendingEvents from "./components/trendingEvents";
 
@@ -8,10 +10,18 @@ const EventPage = () => {
     <>
       <div className="w-screen h-screen bg-WhiteBG dark:bg-BlackBG  overflow-x-hidden">
         <Navbar />
-        <div className="w-full  h-full max-w-7xl mx-auto p-4">
-          <div className="flex flex-col ">
-            <TrendingEvents />
-            {/* <EventOfTheDay /> */}
+        <div className="flex flex-row w-full h-full max-w-7xl  mx-auto p-4">
+          <div className="w-72 max-w-xl h-fit">
+            <EventsFilter />
+          </div>
+
+          <div className="flex flex-col max-w-4xl">
+            <SearchBar />
+            <div className="flex flex-col h-full p-8">
+              <TrendingEvents />
+              <TrendingEvents />
+              <TrendingEvents />
+            </div>
           </div>
         </div>
       </div>
