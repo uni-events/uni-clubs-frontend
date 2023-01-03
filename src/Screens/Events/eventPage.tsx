0110f641c62382cdc97a95634beb5542e4b34fcf
HARI-PRMD <haripramodb@gmail.com>
2023-01-03 20:41:54 +1100

    style: no image repeat

--- src/Screens/Events/eventPage.tsx
+++ src/Screens/Events/eventPage.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Navbar from "../../Components/navbar";
 import SearchBar from "../Discover/components/searchbar";
 // import EventOfTheDay from "./components/eventsOfDay";
@@ -6,19 +7,27 @@ import EventsFilter from "./components/filter";
 import TrendingEvents from "./components/trendingEvents";
 
 const EventPage = () => {
+  const [showFilter, setShowFilter] = useState(false);
   return (
     <>
       <div className="w-screen h-screen bg-WhiteBG dark:bg-BlackBG overflow-x-hidden">
         <Navbar />
-        <div className="flex flex-row w-full h-full max-w-7xl mx-auto grid-cols-3 space-x-4">
-          <div className="w-1/4 h-fit col-span-1 hidden lg:block">
+        <div className="flex flex-row w-full h-full max-w-none mx-auto grid-cols-3 space-x-4">
+          <div
+            className={`w-1/4 h-fit col-span-1 hidden lg:block ${
+              showFilter ? "block" : "hidden md:block"
+            }`}
+          >
             <EventsFilter />
           </div>
 
           <div className="flex flex-col w-full lg:w-3/4 grid-cols-2">
             <div className="flex flex-row space-x-4">
               <SearchBar purpose="Events" />
-              <button className="bg-BlueGrey dark:bg-BlueBlack h-fit w-fit p-3 rounded-lg">
+              <button
+                className="bg-BlueGrey dark:bg-BlueBlack h-fit w-fit p-3 rounded-lg"
+                onClick={() => setShowFilter(!showFilter)}
+              >
                 <svg
                   className="fill-black dark:fill-WhiteBG"
                   xmlns="http://www.w3.org/2000/svg"
