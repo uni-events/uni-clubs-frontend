5045a73b950abb9035f19d5097fb42790f64aaa3
HARI-PRMD <haripramodb@gmail.com>
2023-01-03 20:24:16 +1100

    Fixed Padding Issues with Filter in discover Page

--- src/Screens/Discover/discover.tsx
+++ src/Screens/Discover/discover.tsx
@@ -1,8 +1,8 @@
 import Navbar from "../../Components/navbar";
 import ClubTiles from "./components/ClubTiles/clubTiles";
 import SearchBar from "./components/searchbar";
-import Filter from "./components/filter";
 import { useState } from "react";
+import ClubFilter from "./components/filter";
 
 const Discover = () => {
   const [showFilter, setShowFilter] = useState(false);
@@ -11,9 +11,9 @@ const Discover = () => {
     <>
       <div className="w-screen h-screen bg-WhiteBG dark:bg-BlackBG overflow-x-hidden">
         <Navbar />
-        <div className="flex flex-col max-w-screen-xl mx-auto my-6 md:flex-row ">
-          <div className="w-full block px-4 space-y-4">
-            <div className="flex flex-row space-x-4">
+        <div className="flex flex-col max-w-screen-xl mx-auto md:pr-4 my-6 md:flex-row ">
+          <div className="block w-full px-4 space-y-4">
+            <div className="flex flex-row space-x-4 md:space-x-0">
               <button
                 className="bg-BlueGrey h-fit w-fit p-3 rounded-lg xs:block md:hidden dark:bg-BlueBlack"
                 onClick={() => setShowFilter(!showFilter)}
@@ -27,7 +27,7 @@ const Discover = () => {
                   <path d="M11 21v-6h2v2h8v2h-8v2Zm-8-2v-2h6v2Zm4-4v-2H3v-2h4V9h2v6Zm4-2v-2h10v2Zm4-4V3h2v2h4v2h-4v2ZM3 7V5h10v2Z" />
                 </svg>
               </button>
-              <div className="flex w-full ">
+              <div className="flex w-full">
                 <SearchBar purpose="Clubs" />
               </div>
             </div>
@@ -36,11 +36,11 @@ const Discover = () => {
             </div>
           </div>
           <div
-            className={`px-6 my-6 md:my-0 md:px-0 ${
+            className={`px-4 my-6 md:my-0 md:px-0 ${
               showFilter ? "block" : "hidden md:block"
             }`}
           >
-            <Filter />
+            <ClubFilter />
           </div>
         </div>
       </div>
