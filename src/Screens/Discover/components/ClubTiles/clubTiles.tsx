c5e74a5beed4e62c70d25ed3916186bde29bc78a
HARI-PRMD <haripramodb@gmail.com>
2023-01-09 20:19:09 +1100

    added dummy data for club details

--- src/Screens/Discover/components/ClubTiles/clubTiles.tsx
+++ src/Screens/Discover/components/ClubTiles/clubTiles.tsx
@@ -1,31 +1,117 @@
 import { NavLink } from "react-router-dom";
-import ClubTile from "./clubTile";
+import { longStr } from "../../../../Data/dataTypes";
 
 const ClubTiles = () => {
+  const ClubData: {
+    clubStr: string;
+    name: string;
+    description: string;
+    logo: string;
+    banner: string;
+  }[] = [
+    {
+      clubStr: "catsoc",
+      name: "Cat Appreciation Society",
+      description: longStr,
+      logo: "https://cdn.linkupevents.com.au/society/unswcatsoc.jpg",
+      banner: "https://cdn.linkupevents.com.au/society/unswcatsoc.jpg",
+    },
+    {
+      clubStr: "cserevue",
+      name: "Computer Science and Engineering Revue",
+      description: longStr,
+      logo: "https://cdn.linkupevents.com.au/society/cserevue.jpg",
+      banner: "https://cdn.linkupevents.com.au/society/cserevue.jpg",
+    },
+    {
+      clubStr: "csesoc",
+      name: "CSE Society",
+      description: longStr,
+      logo: "https://cdn.linkupevents.com.au/society/csesoc.jpg",
+      banner: "https://cdn.linkupevents.com.au/society/csesoc.jpg",
+    },
+    {
+      clubStr: "dogsoc",
+      name: "Dog Appreciation Society",
+      description: longStr,
+      logo: "https://cdn.linkupevents.com.au/society/unswdogsoc.jpg",
+      banner: "https://cdn.linkupevents.com.au/society/unswdogsoc.jpg",
+    },
+    {
+      clubStr: "digisoc",
+      name: "Digital Society UNSW",
+      description: longStr,
+      logo: "https://cdn.linkupevents.com.au/society/unswdigitalsociety.jpg",
+      banner: "https://cdn.linkupevents.com.au/society/unswdigitalsociety.jpg",
+    },
+    {
+      clubStr: "catsoc",
+      name: "Cat Appreciation Society",
+      description: longStr,
+      logo: "https://cdn.linkupevents.com.au/society/unswcatsoc.jpg",
+      banner: "https://cdn.linkupevents.com.au/society/unswcatsoc.jpg",
+    },
+    {
+      clubStr: "cserevue",
+      name: "Computer Science and Engineering Revue",
+      description: longStr,
+      logo: "https://cdn.linkupevents.com.au/society/cserevue.jpg",
+      banner: "https://cdn.linkupevents.com.au/society/cserevue.jpg",
+    },
+    {
+      clubStr: "csesoc",
+      name: "CSE Society",
+      description: longStr,
+      logo: "https://cdn.linkupevents.com.au/society/csesoc.jpg",
+      banner: "https://cdn.linkupevents.com.au/society/csesoc.jpg",
+    },
+    {
+      clubStr: "dogsoc",
+      name: "Dog Appreciation Society",
+      description: longStr,
+      logo: "https://cdn.linkupevents.com.au/society/unswdogsoc.jpg",
+      banner: "https://cdn.linkupevents.com.au/society/unswdogsoc.jpg",
+    },
+    {
+      clubStr: "digisoc",
+      name: "Digital Society UNSW",
+      description: longStr,
+      logo: "https://cdn.linkupevents.com.au/society/unswdigitalsociety.jpg",
+      banner: "https://cdn.linkupevents.com.au/society/unswdigitalsociety.jpg",
+    },
+  ];
+
   return (
     <>
       <div className="grid grid-flow-row w-full gap-4 xs:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 ">
-        <NavLink to="/club">
-          <ClubTile />
-        </NavLink>
-        <NavLink to="/club">
-          <ClubTile />
-        </NavLink>
-        <NavLink to="/club">
-          <ClubTile />
-        </NavLink>
-        <NavLink to="/club">
-          <ClubTile />
-        </NavLink>
-        <NavLink to="/club">
-          <ClubTile />
-        </NavLink>
-        <NavLink to="/club">
-          <ClubTile />
-        </NavLink>
-        <NavLink to="/club">
-          <ClubTile />
-        </NavLink>
+        {ClubData.map((club, index) => {
+          return (
+            <NavLink to={`/club`}>
+              <body className="p-2 bg-BlueGrey dark:bg-BlueBlack rounded-lg">
+                <div className="relative">
+                  <img
+                    className="rounded-lg w-full h-24 object-cover mb-6"
+                    src={club.banner}
+                    alt="banner"
+                  />
+
+                  <img
+                    className="absolute h-16 w-16 object-cover left-4 bottom-0 rounded-lg top-16 drop-shadow-lg"
+                    src={club.logo}
+                    alt="logo"
+                  />
+
+                  <div className="text-black whitespace-pre-wrap overflow-hidden p-4 mb-2 max-h-36 dark:text-WhiteBG ">
+                    <h1 className="font-bold text-lg md:text-xl">
+                      {club.name}
+                    </h1>
+                    <h1 className="text-base font-light">{club.description}</h1>
+                  </div>
+                </div>
+              </body>
+            </NavLink>
+          );
+        })}
       </div>
     </>
   );
