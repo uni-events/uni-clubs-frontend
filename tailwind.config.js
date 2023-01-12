e9222a680dec406ce5e9e1a1f77a8415df52375c
HARI-PRMD <haripramodb@gmail.com>
2023-01-12 15:30:46 +1100

    Implemented Light and Dark mode toggle

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
   theme: {
-    darkMode: "media",
     screens: {
       xs: "320px",
       sm: "480px",
@@ -10,6 +10,7 @@ module.exports = {
       lg: "976px",
       xl: "1440px",
     },
+    purge: [],
     extend: {
       spacing: {
         128: "32rem",
