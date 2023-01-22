a011d92366282e027c988c6fa89c12ba51617e26
HARI-PRMD <haripramodb@gmail.com>
2023-01-22 22:15:28 +1100

    added footer to page + no responsiveness issues

--- src/Screens/Admin/Dashboard/adminDashboard.tsx
+++ src/Screens/Admin/Dashboard/adminDashboard.tsx
@@ -8,16 +8,18 @@ import WeeklyTip from "./components/weeklyTip";
 const AdminDashboard = () => {
   return (
     <>
-      <div className="w-full h-full overflow-x-hidden bg-WhiteBG dark:bg-BlackBG duration-ThemeDuration">
-        <AdminNavbar />
-        <div className="flex flex-col content-center w-full max-w-screen-xl gap-8 px-4 mx-auto my-4 lg:flex-row">
-          <div className="flex flex-col w-full space-y-8 lg:w-1/4">
-            <StatisticsCard />
-            <WeeklyTip />
-          </div>
-          <div className="flex flex-col w-full space-y-8 lg:w-3/4 md:col-span-2">
-            <RecentReviews />
-            <EventCard />
+      <div className="flex flex-col justify-between w-full h-screen bg-WhiteBG dark:bg-BlackBG duration-ThemeDuration">
+        <div className="bg-WhiteBG dark:bg-BlackBG duration-ThemeDuration">
+          <AdminNavbar />
+          <div className="flex flex-col content-center w-full max-w-screen-xl gap-8 px-4 mx-auto my-4 h-fit lg:flex-row">
+            <div className="flex flex-col w-full space-y-8 lg:w-1/4">
+              <StatisticsCard />
+              <WeeklyTip />
+            </div>
+            <div className="flex flex-col w-full space-y-8 lg:w-3/4 md:col-span-2">
+              <RecentReviews />
+              <EventCard />
+            </div>
           </div>
         </div>
         <CommonFooter />
--- src/Screens/Admin/ManageEvents/manageEvents.tsx
+++ src/Screens/Admin/ManageEvents/manageEvents.tsx
@@ -5,10 +5,12 @@ import ViewAllEventCards from "./eventCardList";
 const ManageEvents = () => {
   return (
     <>
-      <div className="items-center w-full h-full overflow-x-hidden bg-WhiteBG dark:bg-BlackBG duration-ThemeDuration">
-        <AdminNavbar />
-        <div className="flex flex-col max-w-screen-xl px-4 mx-auto my-12 space-y-8">
-          <ViewAllEventCards />
+      <div className="flex flex-col items-center justify-between w-full h-screen bg-WhiteBG dark:bg-BlackBG duration-ThemeDuration">
+        <div className="bg-WhiteBG dark:bg-BlackBG duration-ThemeDuration">
+          <AdminNavbar />
+          <div className="flex flex-col px-4 mx-auto my-12 space-y-8 h-fit max-w-screen-xxl">
+            <ViewAllEventCards />
+          </div>
         </div>
         <CommonFooter />
       </div>
