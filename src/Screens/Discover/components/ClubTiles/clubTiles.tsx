186651902a04082f70c6fa72644ea90fd6d8291f
HARI-PRMD <haripramodb@gmail.com>
2023-02-06 17:48:55 +1100

    added loading tiles

--- src/Screens/Discover/components/ClubTiles/clubTiles.tsx
+++ src/Screens/Discover/components/ClubTiles/clubTiles.tsx
@@ -14,13 +14,13 @@ const ClubTiles = ({
 }) => {
   let ClubDataInit: ClubTileData[] = [
     {
-      clubStr: "catsoc",
-      name: "Cat Appreciation Society",
-      description: longStr,
+      clubStr: "initclub",
+      name: "Club Name",
+      description: "Club Description",
       logo: "https://cdn.linkupevents.com.au/society/unswcatsoc.jpg",
       banner: "https://cdn.linkupevents.com.au/society/unswcatsoc.jpg",
-      tags: ["exec"],
-      categories: ["Hobby"],
+      tags: [],
+      categories: ["All"],
     },
   ];
   const [ClubData, setClubData] = useState(ClubDataInit);
@@ -70,57 +70,76 @@ const ClubTiles = ({
   return (
     <>
       <div className="grid w-full grid-flow-row gap-4 xs:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
-        {filteredList.map((club, i) => {
-          return (
-            <NavLink key={i} to={`/club/${club.clubStr}`}>
-              <div className="p-2 rounded-lg h-72 bg-BlueGrey dark:bg-BlueBlack hover:scale-[101%] duration-ThemeDuration">
-                <div className="h-[17rem]">
-                  <img
-                    className="object-cover w-full h-24 bg-center rounded-lg "
-                    src={club.banner}
-                    alt="banner"
-                  />
-                  <img
-                    className="relative object-cover w-16 h-16 translate-x-4 -translate-y-8 bg-center rounded-lg drop-shadow-lg"
-                    src={club.logo}
-                    alt="logo"
-                  />
-                  <div className="h-32 px-4 -translate-y-6 ">
-                    <div className="overflow-hidden text-black h-fit dark:text-white">
-                      <h1 className="overflow-x-scroll text-base font-semibold whitespace-nowrap md:text-lg scrollbar-none">
-                        {club.name}
-                      </h1>
-                      <div className="flex flex-row space-x-2 overflow-x-scroll scrollbar-none">
-                        {club.tags.length !== 0 && (
-                          <h1 className="text-sm font-bold">recruiting: </h1>
-                        )}
-                        {club.tags.map((tag, i) => {
-                          return (
-                            <div
-                              key={i}
-                              className="px-2 text-xs font-semibold text-white rounded-lg lg:text-sm bg-Blue w-fit h-fit"
-                            >
-                              {tag === "sub" && "subcommittee"}
-                              {tag === "exec" && "executive"}
-                              {tag === "vol" && "volunteer"}
-                            </div>
-                          );
-                        })}
+        {ClubData[0].clubStr === "initclub"
+          ? [...Array(10)].map((e, i) => (
+              <NavLink to={`/discover`} className="opacity-70">
+                <div className="p-2 rounded-lg h-72 bg-BlueGrey dark:bg-BlueBlack duration-ThemeDuration">
+                  <div className="h-[17rem]">
+                    <div className="object-cover w-full h-24 bg-center rounded-lg bg-slate-400 dark:bg-slate-500" />
+                    <div className="relative object-cover w-16 h-16 translate-x-4 -translate-y-8 bg-center rounded-lg drop-shadow-lg bg-slate-500 dark:bg-slate-400" />
+                    <div className="h-32 px-4 -translate-y-6">
+                      <div className="overflow-hidden h-fit">
+                        <div className="w-full h-6 overflow-x-scroll rounded-lg md:h-7 scrollbar-none bg-slate-400 dark:bg-slate-500" />
                       </div>
+                      <p className="w-full h-16 mt-2 rounded-lg bg-slate-400 dark:bg-slate-500" />
                     </div>
-                    <p
-                      className={`w-full pr-1 overflow-y-scroll text-sm md:text-base text-black whitespace-pre-wrap dark:text-white scrollbar-thin scrollbar-track-transparent scrollbar-thumb-DarkBlueGrey scrollbar-thumb-rounded-2xl ${
-                        club.tags.length === 0 ? "h-4/5 " : "h-3/5 mt-1"
-                      }`}
-                    >
-                      {club.description}
-                    </p>
                   </div>
                 </div>
-              </div>
-            </NavLink>
-          );
-        })}
+              </NavLink>
+            ))
+          : filteredList.map((club, i) => {
+              return (
+                <NavLink key={i} to={`/club/${club.clubStr}`}>
+                  <div className="p-2 rounded-lg h-72 bg-BlueGrey dark:bg-BlueBlack hover:scale-[101%] duration-ThemeDuration">
+                    <div className="h-[17rem]">
+                      <img
+                        className="object-cover w-full h-24 bg-center rounded-lg "
+                        src={club.banner}
+                        alt="banner"
+                      />
+                      <img
+                        className="relative object-cover w-16 h-16 translate-x-4 -translate-y-8 bg-center rounded-lg drop-shadow-lg"
+                        src={club.logo}
+                        alt="logo"
+                      />
+                      <div className="h-32 px-4 -translate-y-6 ">
+                        <div className="overflow-hidden text-black h-fit dark:text-white">
+                          <h1 className="overflow-x-scroll text-base font-semibold whitespace-nowrap md:text-lg scrollbar-none">
+                            {club.name}
+                          </h1>
+                          <div className="flex flex-row space-x-2 overflow-x-scroll scrollbar-none">
+                            {club.tags.length !== 0 && (
+                              <h1 className="text-sm font-bold">
+                                recruiting:{" "}
+                              </h1>
+                            )}
+                            {club.tags.map((tag, i) => {
+                              return (
+                                <div
+                                  key={i}
+                                  className="px-2 text-xs font-semibold text-white rounded-lg lg:text-sm bg-Blue w-fit h-fit"
+                                >
+                                  {tag === "sub" && "subcommittee"}
+                                  {tag === "exec" && "executive"}
+                                  {tag === "vol" && "volunteer"}
+                                </div>
+                              );
+                            })}
+                          </div>
+                        </div>
+                        <p
+                          className={`w-full pr-1 overflow-y-scroll text-sm md:text-base text-black whitespace-pre-wrap dark:text-white scrollbar-thin scrollbar-track-transparent scrollbar-thumb-DarkBlueGrey scrollbar-thumb-rounded-2xl ${
+                            club.tags.length === 0 ? "h-4/5 " : "h-3/5 mt-1"
+                          }`}
+                        >
+                          {club.description}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </NavLink>
+              );
+            })}
       </div>
     </>
   );
