162d612b6aa6594d48586b216132659fb1eee59d
HARI-PRMD <haripramodb@gmail.com>
2023-05-16 19:41:13 +1000

    removed unused sections in home page

--- src/Screens/Home/home.tsx
+++ src/Screens/Home/home.tsx
@@ -15,18 +15,6 @@ const Home = () => {
           <section className="w-screen h-full snap-center">
             <EventInfo />
           </section>
-          <section className="w-screen h-screen bg-blue-200 snap-center">
-            Section 2
-          </section>
-          <section className="w-screen h-screen bg-green-200 full snap-center">
-            Section 3
-          </section>
-          <section className="w-screen h-screen bg-indigo-200 snap-center">
-            Section 4
-          </section>
-          <section className="w-screen h-screen bg-yellow-200 snap-center">
-            Section 5
-          </section>
         </div>
       </div>
     </>
